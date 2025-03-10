6cf51e83820aaa10aadbee8009d5e9f50f382260
1952153 <1952153@01-18111.pusd.dom>
2025-03-10 14:02:15 -0700

    Created example maps & finishing design plan right now

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -3,7 +3,30 @@ public class p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
